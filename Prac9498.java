9e2eac136e7a96f4117763618e61029158b9f231
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-01-23 00:23:00 +0900

    22.01.23.12:22

---
+++ Prac9498.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prac9498 {
+    public static void main(String[] args) { // 숫자를 입력받아 점수에 따라 등급 결정
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("0보다 크거나 같고 100보다 작은수를 입력: ");
+
+        int a;
+        a = scanner.nextInt();
+
+        if(a>=90){
+            System.out.println("A");
+        }else if(a>80){
+            System.out.println("B");
+        }else if(a>=70){
+            System.out.println("c");
+        }else if(a>60){
+            System.out.println("d");
+        }else{
+            System.out.println("F");
+        }
+
+        scanner.close();
+    }
+}
